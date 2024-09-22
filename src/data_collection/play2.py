d71f699be83929498cf75c6777b1ee67213dc33f
Sid3024 <siddharth.arum@gmail.com>
2024-09-22 18:07:08 +0800

    update fen_to_vector

--- src/data_collection/play2.py
+++ src/data_collection/play2.py
@@ -1,8 +1,10 @@
 def fen_to_vector(fen):
     fen_parts = fen.split(" ")
+    rows = fen_parts[0].split("/")
+    turn = fen_parts[1]
     if fen_parts[1] == "b":
         
-        fen_parts[0] = fen_parts[0][::-1].swapcase()
+        rows = [row.swapcase() for row in rows][::-1]
         
         fen_parts[2] = fen_parts[2].swapcase()
         #fen_parts[1] = 1
@@ -10,7 +12,8 @@ def fen_to_vector(fen):
     #     #fen_parts[1] = 0
     position="0," #special token
     piece_dict = {" ":"1,", "p":"2,", "n":"3,", "b":"4,", "r":"5,", "q":"6,", "k":"7,", "P":"8,", "N":"9,", "B":"10,", "R":"11,", "Q":"12,", "K":"13,"}
-    for row in fen_parts[0].split("/"):
+    print(f"{rows=}")
+    for row in rows:
         for square in row:
             if square.isalpha():
                 position+=piece_dict[square]
@@ -32,7 +35,32 @@ def fen_to_vector(fen):
         position += "0," * (7 - file_index)
 
 
-    return position[:-1]
+    return position[:-1], turn
 
-print(fen_to_vector("2kr1bnr/p1p3pp/2nppq2/4p3/Pp1PP3/2N2N1P/1PPB1PP1/R2Q1RK1 b - - 0 1"))
+def flip_uci(uci_move_string):
+    out = ""
+    out+=uci_move_string[0]
+    out+=str(9 - int(uci_move_string[1]))
+    out+=uci_move_string[2]
+    out+=str(9 - int(uci_move_string[3]))
+    return out
+
+
+
+
+
+
+
+
+
+
+
+print(flip_uci("e1g1"))
+print(fen_to_vector("2kr1bnr/p1p3pp/2nppq2/4p3/Pp1PP3/2N2N1P/1PPB1PP1/R2Q1RK1 b Kq a3 0 1"))
 print(fen_to_vector("2kr1bnr/p1p3pp/2nppq2/Pp2p3/3PP3/2N2N1P/1PPB1PP1/R2Q1RK1 w Qk b6 0 1"))
+
+
+
+
+
+#out+=(chr(97 + 7 - (ord(uci_move_string[2]) - 97)))
