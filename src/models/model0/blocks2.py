250d66d2bb1bb1cd4599cc6ff68164e8caa1708b
Sid3024 <siddharth.arum@gmail.com>
2024-11-14 12:20:49 +0800

    updating att/smolgen

--- src/models/model0/blocks2.py
+++ src/models/model0/blocks2.py
@@ -69,6 +69,40 @@ class CausalSelfAttention(nn.Module):
         y = self.c_proj(y)
         return y
 
+
+class CausalSelfAttentionv2(nn.Module):
+
+    def __init__(self, model_config, token_to_a2):
+        super().__init__()
+        assert model_config.n_embd % model_config.n_head == 0
+        # key, query, value projections for all heads, but in a batch
+        self.c_attn = nn.Linear(model_config.n_embd, 3 * model_config.n_embd)
+        # output projection
+        self.c_proj = nn.Linear(model_config.n_embd, model_config.n_embd)
+        # regularization
+        self.n_head = model_config.n_head
+        self.n_embd = model_config.n_embd
+        self.token_to_a2 = token_to_a2
+
+    def forward(self, x):
+        B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
+        # calculate query, key, values for all heads in batch and move head forward to be the batch dim
+        # nh is "number of heads", hs is "head size", and C (number of channels) = nh * hs
+        # e.g. in GPT-2 (124M), n_head=12, hs=64, so nh*hs=C=768 channels in the Transformer
+        qkv = self.c_attn(x)
+        q, k, v = qkv.split(self.n_embd, dim=2)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        board_overview_token = x[:, 0:1, :].squeeze(1) #(B, n_embd)
+        a2 = self.token_to_a2(board_overview_token).view(B, self.n_head, T, T) # (B, nh, T2, T)
+        y = F.scaled_dot_product_attention(q, k, v, attn_mask=a2) # flash attention, is_causal=False cos no ,masking
+        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+        # output projection
+        y = self.c_proj(y)
+        return y
+
+
 class MLP(nn.Module):
 
     def __init__(self, model_config):
@@ -85,10 +119,10 @@ class MLP(nn.Module):
 
 class Block(nn.Module):
 
-    def __init__(self, model_config):
+    def __init__(self, model_config, token_to_a2):
         super().__init__()
         self.ln_1 = nn.LayerNorm(model_config.n_embd)
-        self.attn = CausalSelfAttention(model_config)
+        self.attn = CausalSelfAttention(model_config, token_to_a2)
         self.dropout_1 = nn.Dropout(model_config.dropout)
         self.ln_2 = nn.LayerNorm(model_config.n_embd)
         self.mlp = MLP(model_config)
@@ -156,9 +190,11 @@ class Transformer(nn.Module):
             self.pte = nn.Embedding(8, model_config.n_embd)
             self.ce = nn.Embedding(4, model_config.n_embd)
         self.pe = nn.Embedding(model_config.squares_size, model_config.n_embd)
-        self.h = nn.ModuleList([Block(model_config) for _ in range(model_config.n_layer)])
+        self.token_to_a2 = nn.Linear(model_config.n_embd, (model_config.total_tokens)**2)
+        self.h = nn.ModuleList([Block(model_config, self.token_to_a2) for _ in range(model_config.n_layer)])
         self.ln_f = nn.LayerNorm(model_config.n_embd)
         self.cls_emb = nn.Parameter(torch.randn(model_config.n_embd)).to(device)
+        
         
     def forward(self, board_state_tensor, special_tokens_tensor):
         # idx is of shape (B, T)
