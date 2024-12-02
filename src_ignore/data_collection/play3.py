92ae28b2cded1ba20e3ecf7603524955492e7320
Sid3024 <siddharth.arum@gmail.com>
2024-12-02 22:12:46 +0800

    Update play3.py

--- src_ignore/data_collection/play3.py
+++ src_ignore/data_collection/play3.py
@@ -18,12 +18,23 @@ def my_attn(N, d, H, D, w, M):
     out = local_attn(N, d, H, D, w, M) + global_attn(N, d, H, D, w, M) 
     return out
 
-N = 16384
+def bigbird_attn(N, d, H, D, w, M, K):
+    out = 8*N*d*d + (4*d+5)*N*M*K + (4*d+10)*N*M + (21*d+10)*N
+    return out
+
+def output_layer(d, dict_size):
+    out = 2*dict_size*d
+    return out
+
+
+N = 4096
 L = 12
 d = 768
 H = 12
-D = 8192
+D = 4096
 w = 64
+K = 8
+dict_size = 30000
 assert N % w == 0
 M = N // w
 #assert D // M == 64
@@ -34,18 +45,26 @@ Dense = int(dense_attn(N, d, H, D, w, M) / (1000000000))
 Local = int(local_attn(N, d, H, D, w, M) / (1000000000))
 Global = int(global_attn(N, d, H, D, w, M) / (1000000000))
 Mine = int((Local + Global))
+BigBird = int(bigbird_attn(N, d, H, D, w, M, K) / (1000000000))
 MLP_out = int(MLP(N, d, H, D, w, M) / (1000000000))
 Block = Dense + MLP_out
 MyBlock = Mine + MLP_out
-Total = Block * L
-MyTotal = MyBlock * L
+BigBirdBlock = BigBird + MLP_out
+Output = int(output_layer(d, dict_size) / (1000000000))
+Total = Block * L + Output
+MyTotal = MyBlock * L + Output
+BigBirdTotal = BigBirdBlock * L + Output
 
+print(f"{N=}, {L=}, {d=}, {H=}, {D=}, {w=}, {K=}")
 print(f"{Dense=}")
 print(f"{Local=}")
 print(f"{Global=}")
 print(f"{Mine=}")
+print(f"{BigBird=}")
 print(f"{Block=}")
 print(f"{MyBlock=}")
+print(f"{BigBirdBlock=}")
 print(f"{Total=}")
 print(f"{MyTotal=}")
+print(f"{BigBirdTotal=}")
 
